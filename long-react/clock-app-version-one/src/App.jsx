b91c2a4b27abef02a4df89034ccb2f1f69522b8d
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-21 09:34:07 +0530

    clock version one done

---
+++ long-react/clock-app-version-one/src/App.jsx
@@ -0,0 +1,21 @@
+
+import Subtext from "./Components/Subtext"
+import Time from "./Components/Time"
+import Title from "./Components/title"
+import "./App.css"
+
+function App() {
+  
+
+  return (
+    <>
+      <center>
+      <Title></Title>
+      <Subtext></Subtext>
+      <Time></Time>
+      </center>
+    </>
+  )
+}
+
+export default App
