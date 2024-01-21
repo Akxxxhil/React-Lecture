f74b1525d09a76286cb1161b183f12324c2b8e44
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-21 12:27:49 +0530

    Fragemnt done

---
+++ long-react/Fragments/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h2>Healthy Food Item</h2>
+      <ul class="list-group">
+        <li class="list-group-item">Rice</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+// After writting the react fragement i dont need any extra div or dom for showcase
