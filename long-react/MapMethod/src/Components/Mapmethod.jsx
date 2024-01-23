593a859c7dae1ba92e8f96cca7251ffd86f6de7a
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-23 11:08:57 +0530

    conditional rendering added

--- long-react/MapMethod/src/Components/Mapmethod.jsx
+++ long-react/MapMethod/src/Components/Mapmethod.jsx
@@ -1,8 +1,13 @@
 function Mapmethod() {
+
+  //let foodItems = [];
   let foodItems = ["Rice", "Dal","Milk","Fruits"];
   return (
     <>
       <div>
+        <h2>Healthy Food</h2>
+        {/* This is called a s conditional Rendering */}
+        {foodItems.length===0 && <h4>I am still Hungry.</h4>}
         <ul className="list-group">
            {foodItems.map((item)=>(
             <li key={item} className="list-group-item">{item}</li>
