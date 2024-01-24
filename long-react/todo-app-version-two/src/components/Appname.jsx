445d948190750927bcb59cb2179738419825b56e
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-24 12:54:36 +0530

    version 2 updated

--- long-react/todo-app-version-two/src/components/Appname.jsx
+++ long-react/todo-app-version-two/src/components/Appname.jsx
@@ -1,9 +1,11 @@
-function Appname(){
-return <>
-<div className="head-container">
-
-<h1>Todo App</h1>
-</div>
-</>
+import styles from "./Appname.module.css";
+function Appname() {
+  return (
+    <>
+      <div>
+        <h1 className={styles.headcontainer}>Todo App</h1>
+      </div>
+    </>
+  );
 }
-export  default Appname
+export default Appname;
