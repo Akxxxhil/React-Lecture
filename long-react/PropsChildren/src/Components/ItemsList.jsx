7da893ac85a534d63cedacba1bdc3334e0038a66
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-25 13:27:31 +0530

    passing as children

---
+++ long-react/PropsChildren/src/Components/ItemsList.jsx
@@ -0,0 +1,12 @@
+function ItemsList(){
+let Listitems=["Rice","Roti","Dal","Milk","Salad"];
+return <>
+<ul className="list-group">
+{Listitems.map((item)=>(
+    <li key={item} className="list-group-item">{item}</li>
+))}
+</ul>
+</>
+}
+
+export default ItemsList;
