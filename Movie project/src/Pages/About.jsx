beca7c61c5ce1dd3ab56e3f7ccadde90083b8e35
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-04-07 09:33:29 +0530

    depricated

--- Movie project/src/Pages/About.jsx
+++ Movie project/src/Pages/About.jsx
@@ -1,8 +1,27 @@
-function About(){
+function About() {
     return <>
-    <div>
-        This is About Page
-    </div>
+        <section id="about">
+            <div class="container">
+                <h1>About MovieTube</h1><br />
+
+                <p>Welcome to MovieTube, your ultimate destination for the latest news and updates on Bollywood movies! At MovieTube, we are passionate about providing movie enthusiasts with comprehensive information about the newest releases, upcoming films, trailers, reviews, and much more.</p><br /><br /><br />
+
+                <p>Our platform is designed to be your one-stop-shop for everything Bollywood, catering to the diverse tastes and preferences of our users. Whether you're a die-hard fan or just looking for something new to watch, MovieTube has got you covered.</p><br /><br />
+
+                <h3>Key Features:</h3><br />
+
+                <ul style={{display:"flex",flexDirection:"column"}}>
+                    <li>Comprehensive Movie Listings</li>
+                    <li>Personalized Favorites List</li>
+                    <li>User-Friendly Interface</li>
+                    <li>Interactive Community</li>
+                    <li>Convenient Movie Management</li>
+                </ul><br /><br />
+
+                <p>At MovieTube, we are committed to providing our users with the most accurate and up-to-date information, empowering them to make informed decisions about their movie-watching experience. Join us on this cinematic journey and discover the magic of Bollywood like never before!</p>
+            </div>
+        </section>
+
     </>
 }
 export default About
--- Movie project/src/Pages/Career.jsx
+++ Movie project/src/Pages/Career.jsx
@@ -1,8 +1,19 @@
 function Career (){
     return <>
-    <div>
-       This is Career Page
+   <section id="career">
+    <div class="container">
+        <h2>Career Opportunities</h2><br />
+        <p>Are you passionate about technology and movies? Do you thrive in a dynamic and collaborative work environment? If so, MovieTube is looking for talented individuals like you to join our team as Software Engineers.</p><br /><br />
+        <h3>Software Engineer 1</h3><br />
+        <p>As a Software Engineer 1 at MovieTube, you will play a key role in developing and maintaining our innovative platform.</p><br /><br />
+        
+        <h3>Software Engineer 2</h3><br />
+        <p>As a Software Engineer 2 at MovieTube, you will take on additional responsibilities and lead initiatives to drive the evolution of our platform.</p>
+        
+        <p>At MovieTube, we value diversity, creativity, and innovation. Join us in revolutionizing the way people experience Bollywood movies and be a part of our exciting journey towards success!</p>
+        <p>If you're interested in joining our team, please send your resume and cover letter to [email address]. We look forward to hearing from you!</p>
     </div>
+</section>
     </>
 }
 export default Career
