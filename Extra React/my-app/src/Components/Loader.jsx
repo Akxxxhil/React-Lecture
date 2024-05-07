70b6f786482117bb9febaab0437d346dcbe851f8
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-05-07 13:19:31 +0530

    shimmer effect

--- Extra React/my-app/src/Components/Loader.jsx
+++ Extra React/my-app/src/Components/Loader.jsx
@@ -3,11 +3,14 @@ import Display from './Display'
 
 function Loader() {
   return (
-    <div>
-      <div style={{display:'flex',flexDirection:"column",justifyContent:"center",alignItems:"center",position:"relative",top:"50%"}}>
+    <div style={{display:"flex",flexWrap:"wrap",padding:"10px",margin:"10px"}}>
+      {/* <div style={{display:'flex',flexDirection:"column",justifyContent:"center",alignItems:"center",position:"relative",top:"50%"}}>
       <div className="spinner"></div>
       <p>Loading...</p>
-      </div>
+      </div> */}
+      {/* shimmer effect */}
+      {Array(10).fill("").map(e=><div style={{width:"300px",height:"100px",border:"1px solid green"}}></div>)}
+      
     </div>
   )
 }
